62eb8e9a08098f825d6675f248aafd0311e38deb
Seamus O'Keefe <okeefe77@gmail.com>
2015-10-19 16:16:57 +0000

    Solutions to problems 6A-6C

--- unit-01/problem-sets/ps-03/ps3b.py
+++ unit-01/problem-sets/ps-03/ps3b.py
@@ -3,6 +3,23 @@ import time
 from perm import *
 
 
+def find_words(aggregator, hand, wordlen, word_set):
+    """
+    Recursively accumulates set of valid words made from the given hand
+
+    aggregator: list (should be empty list in manual call)
+    hand: dictionary (string -> int)
+    wordlen: int (should be number of letters in hand to start)
+    word_set: set of valid words
+    """
+    if wordlen == 0:
+        return aggregator
+
+    else:
+        new_words = set(get_perms(hand, wordlen))
+        return aggregator + list(word_set & new_words) + \
+            find_words(aggregator, hand, wordlen - 1, word_set)
+
 #
 #
 # Problem #6A: Computer chooses a word
@@ -16,7 +33,10 @@ def comp_choose_word(hand, word_list):
     hand: dictionary (string -> int)
     word_list: list (string)
     """
-    # TO DO...
+    words = find_words([], hand, calculate_handlen(hand), set(word_list))
+
+    if len(words) > 0:
+        return words[0]
 
 #
 # Problem #6B: Computer plays a hand
@@ -29,8 +49,8 @@ def comp_play_hand(hand, word_list):
 
      * The computer chooses a word using comp_choose_words(hand, word_dict).
 
-     * After every valid word: the score for that word is displayed, 
-       the remaining letters in the hand are displayed, and the computer 
+     * After every valid word: the score for that word is displayed,
+       the remaining letters in the hand are displayed, and the computer
        chooses another word.
 
      * The sum of the word scores is displayed when the hand finishes.
@@ -40,8 +60,26 @@ def comp_play_hand(hand, word_list):
      hand: dictionary (string -> int)
      word_list: list (string)
     """
-    # TO DO ...    
-    
+    total_score = 0
+    while calculate_handlen(hand) > 0:
+        print
+        print 'Current hand: {}'.format(get_displayable_hand(hand))
+        word = comp_choose_word(hand, word_list)
+
+        if word == None:
+            print 'Computer could not find a word.'
+            break
+        else:
+            print 'Computer plays: {}'.format(word)
+            word_score = get_word_score(word, calculate_handlen(hand))
+            total_score += word_score
+            print '"{0}" scored {1:d} points. Total: {2:d} points'.format(
+                word, word_score, total_score)
+            hand = update_hand(hand, word)
+
+    print
+    print 'Total score: {0:d} points.'.format(total_score)
+
 #
 # Problem #6C: Playing a game
 #
@@ -64,8 +102,45 @@ def play_game(word_list):
 
     word_list: list (string)
     """
-    # TO DO...
-        
+    hand = {}
+
+    while True:
+        user_action = ''
+        print
+        print
+        print 'Main Menu:'
+        print '----------'
+        print 'n - deals a new, random hand'
+
+        if calculate_handlen(hand) > 0:
+            print 'r - replays the previous hand'
+
+        print 'e - exits'
+        print
+        user_action = raw_input("Your choice: ")
+
+        if user_action == 'e':
+            print "Goodbye!"
+            break
+
+        if user_action == 'n':
+            hand = deal_hand(HAND_SIZE)
+
+        if user_action == 'r' or user_action == 'n':
+            player_choice = ''
+            while player_choice not in ('u', 'c'):
+                print
+                print 'Who plays?'
+                print 'u - you'
+                print 'c - computer'
+                player_choice = raw_input('Your choice: ')
+
+                if player_choice == 'u':
+                    play_hand(hand, word_list)
+
+                elif player_choice == 'c':
+                    comp_play_hand(hand, word_list)
+
 #
 # Build data structures used for entire session and play game
 #
@@ -73,4 +148,4 @@ if __name__ == '__main__':
     word_list = load_words()
     play_game(word_list)
 
-    
+
